2805c7f43db45b65f7286915089c2ce513ddb393
kpgraham <kpgraham@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-26 16:54:38 +0000

    git-svn-id: https://plugins.svn.wordpress.org/permalink-finder/trunk@167275 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- permalink-finder.php
+++ permalink-finder.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Permalink Finder
-Plugin URI: http://www.BlogsEye.com/PermaLinks-Finder/
-Description: When you migrate from another platform to Wordpress, the canonical names of your posts may subtly change. Old links, including Google may throw 404 errors on your permalinks. In order to redirect your valuable links to the new naming structure, you will need some way of locating the poast based on the information available in the old link.
-Version: 1.0
+Plugin URI: http://www.BlogsEye.com/permalink-finder/
+Description: When you migrate from another platform to Wordpress, the canonical names of your posts may subtly change. Old links, including Google may throw 404 errors on your permalinks. In order to redirect your valuable links to the new naming structure, you will need some way of locating the poast based on the information available in the old link. This version adds some configuration control and redirects index pages.
+Version: 1.1
 Author: Keith P. Graham
 Author URI: http://www.BlogsEye.com/
 
@@ -22,19 +22,40 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 *	possible.
 *************************************************************/
 function kpg_permalink_finder() {
- 
 	if( is_404() ) {
-		$plink = basename( $_SERVER['REQUEST_URI'] ); // plink has the page that was 404'd	 
-		if( $ID = kpg_find_permalink_post( $plink ) ) { //check for match	
-			//logError("permalink=".get_permalink( $ID ) );
-			kpg_301_forward( get_permalink( $ID ) ); // if match forward it.
-			return false;
+		// get the configuration
+		$updateData=get_option('kpg_permalinfinder_options');
+		if ($updateData==null) $updateData=array();
+		$kpg_pf_find=$updateData['find']; // used to tell if permalink fixing is going on
+		$kpg_pf_index=$updateData['index']; // used to tell if we are redirecting index pages
+		// check data and set defaults
+		if ($kpg_pf_find==null) $kpg_pf_find='2';
+		if ($kpg_pf_find!='9999' && $kpg_pf_find!='1' && $kpg_pf_find!='2' && $kpg_pf_find!='3' && $kpg_pf_find!='4') {
+			$kpg_pf_find='2';
+		}
+		if ($kpg_pf_index==null) $kpg_pf_index='';
+		if ($kpg_pf_index!='') $kpg_pf_index='Y';
+
+		$furl=$_SERVER["SCRIPT_URI"];
+		$burl=get_bloginfo('url');
+		// check to see if the user is coming in on a base default
+		
+		if ($kpg_pf_index=='Y') { // quick check to see if we are accessing an index page
+			if ($furl==$burl.'/index.html'||$furl==$burl.'/index.htm'||$furl==$burl.'/index.shtml'||$furl==$burl.'/default.asp') {
+					kpg_301_forward(get_bloginfo('url'));
+			}
+		}
+
+		$plink = basename( $_SERVER['REQUEST_URI'] ); // plink has the page that was 404'd	
+		// now figure if we need to fix a permalink
+		if ($kpg_pf_find<5) {
+			if( $ID = kpg_find_permalink_post( $plink,$kpg_pf_find ) ) { //check for match	
+				kpg_301_forward( get_permalink( $ID ) ); // if match forward it.
+				return false;
+			}
 		}
 	}
 }
-
-
-
 /************************************************************
 *	kpg_301_forward( $post_new_location )
 *	$post_new_location is the new URL.
@@ -44,23 +65,17 @@ function kpg_permalink_finder() {
 *************************************************************/
 function kpg_301_forward( $post_loc ) {
 	header( "HTTP/1.1 301 Moved Permanently" );
-	//logError("Trying to relocate to ".post_loc);
-	//echo "Trying to relocate to ".$post_loc;
 	header( "Location: $post_loc" );
 	exit();
 }
-
-
 /************************************************************
 *	kpg_permalink_finder( $plink )
 *	$plink is the permalink value from the url
 *	does a lookup on the posts table in order to find the post
 *	if not found in posts it tries the pages table.
 *************************************************************/
-function kpg_find_permalink_post( $plink ) {
- 
- 	global $wpdb; // useful db functions
-	
+function kpg_find_permalink_post( $plink,$kpg_pf_find ) {
+	global $wpdb; // useful db functions
 	$plink=strtolower($plink); // make it case insensitive
 	$plink=str_replace('.html','',$plink); // get dir of html and shtml at the end - don't need to search for these
 	$plink=str_replace('.shtml','',$plink); 
@@ -73,28 +88,29 @@ function kpg_find_permalink_post( $plink ) {
 	// look for each word in the array. If found add in 1; if not add in 0. Order by sum and the best bet bubbles to top.
 	$sql="SELECT ID, ";
 	for ($j=0;$j<count($ss);$j++) {
-		$sql=$sql." if(INSTR(LCASE(post_name),'".$ss[$j]."'),1,0)+" ;
+		$sql=$sql." if(INSTR(LCASE(post_name),'".mysql_real_escape_string($ss[$j])."'),1,0)+" ;
 	}
-	$sql=$sql."0 as CNT FROM ".$wpdb->posts." WHERE post_status = 'publish' ORDER BY CNT DESC";
+	$sql=$sql."0 as CNT FROM ".$wpdb->posts." WHERE post_status = 'publish' ORDER BY CNT DESC, POST_DATE DESC";
 	$row=$wpdb->get_row($sql);
 	if ($row) {	
 	   $ID=$row->ID; 
 	   $CNT=$row->CNT;
-	   if ($CNT>0) return $ID;
-	}
+	   if ($CNT>=$kpg_pf_find) return $ID;
+	} 
 	return false;
 }
 
-function logError($msg) {
-	// used during debugging
-	 $f = fopen( 'kpgerrors.txt', "a" );
-	 $d=date('Y/m/d H:i:s');
-	 fwrite($f,"$d: check mover $msg \r\n");
-	 fclose($f);
 
+/************************************************************
+*	kpg_permalink_finder_admin_menu()
+*	Adds the admin menu
+*************************************************************/
+function kpg_permalink_finder_admin_menu() {
+   add_options_page('Permalink Finder', 'Permalink Finder', 'manage_options', 'permalink-finder/permalink-finder-options.php');
 }
 
-  // Plugin added to Wordpress plugin architecture
 add_action( 'template_redirect', 'kpg_permalink_finder' );
+// add the the options to the admin menu
+add_action('admin_menu', 'kpg_permalink_finder_admin_menu');
 
 ?>
--- readme.txt
+++ readme.txt
@@ -1,35 +1,42 @@
 === Permalink Finder Plugin ===
-Tags: permalinks, move, migrate, 301, 404, redirect, PageRank, google, seo, forward
-Stable tag: trunk
-Requires at least: 2.0
-Tested up to: 2.8
-Contributors: Keith Graham
-Stable tag: 1.0
+Tags: permalinks, move, migrate, 301, 404, redirect, PageRank, google, seo, forward       
+Stable tag: trunk       
+Requires at least: 2.0       
+Tested up to: 2.8.5
+Contributors: Keith Graham       
+Stable tag: 1.1
 
-
-Detects 404 errors and does a search for pages that partially match the url entered. Fixes mangled or altered permalinks.
+Detects 404 errors and does a search for pages that partially match the url entered. Fixes mangled or altered permalinks and index files.
 
 == Description ==
 
 The Permalink Finder Plugin detects when Wordpress cannot find a permalink. Before it generates the 404 error it tries to locate any posts with similar words. It does this by searching through the database trying to find any of the word values from the bad link. It takes the best match and then, rather than issuing a 404 error it sends back a redirect to the correct page.
+Users will see the page that they are looking for, and search engine spiders will see the 301 redirect and update their databases so that the page appears correctly in searches.
 
-Users will see the page that they are looking for, and search engine spiders will see the 301 redirect and update their databases so that searchers will be linked to the correct page.
-
-This is especially useful where Wordpress removes words like "the" and "a" from the permalink during conversions from Blogger accounts. It is also useful for migrations that formerly used extensions such as html and shtml, when wordpress does not.
+This is especially useful where Wordpress removes words like "the" and "a" from the permalink during conversions from Blogger accounts. It is also useful for migrations that formerly used extensions such as html and shtml, when Wordpress does not.
 
 The search of the database requires a small amount of extra overhead, but it only occurs when Wordpress cannot find the original post and resorts to using this plugin, which should be rare, especially after the search engines fix up their own indexes.
 
-Currently there are no configuration options. 
+The configuration panel allows a user to select how the plugin finds a missing page. The plugin counts the number of words that match to a post. By default, a two word match is sufficient to cause a redirect to the found page. False positives are possible, especially if the user selects a one word match. Increasing the number of words, however makes it unlikely that the plugin will ever find a match.
 
-This plugin was originally a modification of the popular permalinks-moved-permanently Plugin, which is a good plugin for most uses. I had some extra needs and started modifying it until it no longer resembled the original. My thanks to Microkid at http://www.microkid.net/wordpress/permalinks-moved-permanently/ for producing such a nice design, which gave me my first lessons on how to write a plugin.
+Optionally, the plugin will redirect hits on index.html, index.htm, and index.shtml to the blog home page. This is useful when a website previously used a non-php home page.
 
 == Installation ==
 
 1. Download the plugin.
 2. Upload the plugin to your wp-content/plugins directory.
 3. Activate the plugin.
-
 The plugin can be tested by adding or deleting words from a working permalink in your browser address area. Even if you mangle the permalink it should find a valid link and almost always it will find the correct link.
+
+== Changelog ==
+
+= 1.0 =
+* initial release 
+
+= 1.1 =
+* added ability select degree of matching on bad urls.
+* added the ability to redirect index.htm, index.html and index.shtml to blog home page.
+* fixed a stupid name in the install directory - should be "permalink-finder" no s.
 
 == Support ==
 

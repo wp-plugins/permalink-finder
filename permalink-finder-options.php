40e9731a742fd461a1a415146673cb954ec92dc8
kpgraham <kpgraham@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-14 19:44:44 +0000

    git-svn-id: https://plugins.svn.wordpress.org/permalink-finder/tags/1.60@332606 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ permalink-finder-options.php
@@ -0,0 +1,260 @@
+<?php
+/*
+	WordPress 3.1 Plugin: Permalink-Finder 1.60 				
+	Copyright (c) 2011 Keith P. Graham 	
+ 
+	File Information:  					
+	- Permalink-Finder				
+	- wp-content/plugins/permalink-finder/permalink-finder-options.php 	
+ 
+*/
+
+// just a quick check to keep out the riff-raff
+if(!current_user_can('manage_options')) {
+	die('Access Denied');
+}
+// defaults if value on update is null
+$kpg_pf_find='2'; 
+$kpg_pf_index='N';
+$kpg_pf_stats='0';
+$kpg_pf_labels='N';
+$kpg_pf_mu='Y';
+$e404=array();
+$f404=array();
+// get the options file
+$options=get_option('kpg_permalinfinder_options');
+if (empty($options)) $options=array();
+// set all the params
+if (array_key_exists('find',$options)) $kpg_pf_find=$options['find'];
+if (array_key_exists('index',$options)) $kpg_pf_index=$options['index'];
+if (array_key_exists('stats',$options)) $kpg_pf_stats=$options['stats'];
+if (array_key_exists('labels',$options)) $kpg_pf_labels=$options['labels'];
+if (array_key_exists('mu',$options)) $kpg_pf_mu=$options['mu'];
+// history files
+if (array_key_exists('e404',$options) ) $e404=$options['e404']; 
+if (array_key_exists('f404',$options) ) $f404=$options['f404']; 
+global $blog_id;
+
+// see if we are getting anything from the admin update post.
+if(!empty($_POST['Submit'])&&wp_verify_nonce($_POST['kpg_pf_nonce'],'kpg_pf') ) {
+	// data:
+	//		kpg_pf_find: this is a radio box that indicates if the finder should be working or not default = true
+	if (array_key_exists('kpg_pf_find',$_POST) ){
+		$kpg_pf_find = $_POST['kpg_pf_find'];
+	}
+	// kpg_pf_find is 999,1,2,3, or 4
+	if ($kpg_pf_find!='9999' && $kpg_pf_find!='1' && $kpg_pf_find!='2' && $kpg_pf_find!='3' && $kpg_pf_find!='4') {
+		$kpg_pf_find='2';
+	}
+	//		kpg_pf_index: fix up incoming hits with index.html 
+	if (array_key_exists('kpg_pf_index',$_POST) ){
+		$kpg_pf_index = $_POST['kpg_pf_index'];
+	} else {
+		$kpg_pf_index='N';
+	}
+
+	if ($kpg_pf_index!='Y' && $kpg_pf_index!='N') $kpg_pf_index='N';
+	//		kpg_pf_labels: fix up blogger labels folder 
+	if (array_key_exists('kpg_pf_labels',$_POST)) {
+		$kpg_pf_labels = $_POST['kpg_pf_labels'];
+	} else {
+		$kpg_pf_labels='N';
+	}
+
+	// labels can be Y or N
+	if ($kpg_pf_labels!='Y' && $kpg_pf_labels!='N') $kpg_pf_labels='N';
+	//		kpg_pf_stats: this is a radio box that indicates if the finder should be working or not default = true
+	if (array_key_exists('kpg_pf_stats',$_POST) ){
+		$kpg_pf_stats = $_POST['kpg_pf_stats'];
+	} 
+	// stats van be 0,10,20, or 30
+	if ($kpg_pf_stats!='10' && $kpg_pf_stats!='20' && $kpg_pf_stats!='30') {
+		$kpg_pf_stats='0';
+	}
+	// mu function can only be set on main page
+	if (function_exists('switch_to_blog')) {
+		if ($blog_id==1) {
+			if (array_key_exists('kpg_pf_mu',$_POST)) {
+				$kpg_pf_mu = $_POST['kpg_pf_mu'];
+			} else {
+				$kpg_pf_mu='N';
+			}
+		} 
+	} 
+	// labels can be Y or N
+	if ($kpg_pf_mu!='Y' && $kpg_pf_mu!='N') $kpg_pf_mu='N';
+	
+	// out in data array
+	$options['find']=$kpg_pf_find;
+	$options['index']=$kpg_pf_index;
+	$options['stats']=$kpg_pf_stats;
+	$options['labels']=$kpg_pf_labels;
+	$options['mu']=$kpg_pf_mu;
+	if ($kpg_pf_stats=='0') { 
+		// clear out any statistics
+		if (array_key_exists('f404',$options) ) unset($options['f404']);
+		if (array_key_exists('e404',$options) ) unset($options['e404']);
+		$e404=array();
+		$f404=array();
+	}
+	// save the results in repository
+	update_option('kpg_permalinfinder_options', $options);
+	// done
+}	
+?>
+
+<div class="wrap">
+<h2>Permalink-Finder Options </h2>
+
+<form method="post" action="">
+<input type="hidden" name="action" value="update" />
+<input type="hidden" name="page_options" value="Submit,kpg_pf_find,kpg_pf_index,action" />
+
+	<?php wp_nonce_field( 'kpg_pf', 'kpg_pf_nonce' ) ?> 
+<table class="form-table">
+<tr valign="middle">
+<td>Finding Permalinks </td>
+	<td> 
+	  <select name="kpg_pf_find">
+		<option value="9999" <?php if ($kpg_pf_find=='9999') {?> selected="selected" <?php } ?>>Disabled</option>
+		<option value="1" <?php if ($kpg_pf_find=='1') {?> selected="selected" <?php } ?>>any single word match</option>
+		<option value="2" <?php if ($kpg_pf_find=='2') {?> selected="selected" <?php } ?>>at least 2 words match (recomended)</option>
+		<option value="3" <?php if ($kpg_pf_find=='3') {?> selected="selected" <?php } ?>>at least 3 words match</option>
+		<option value="4" <?php if ($kpg_pf_find=='4') {?> selected="selected" <?php } ?>>at least 4 words match</option>
+	  </select>	</td>
+	<td>Indicate how many words in the bad url must match a real permalink.<br/>
+	For instance: if the mistaken link is "a-list-of-games" this will find a post called "list-of-games" or "games-list". <br/>Matching any single word might redirect to a totally unrelated post, but if you ask for 4 matches you will never be able to fix links with only three words.<br/>This is useful for people who have imported a Blogger.com FTP site into Wordpress.	</td>
+</tr>
+<tr valign="middle">
+	<td>Redirect index.html to your blog main page. </td>
+	<td><input name="kpg_pf_index" type="checkbox" value="Y" <?php if ($kpg_pf_index=='Y') {?> checked="checked" <?php } ?>/></td>
+	<td>If your have incoming links to index.html, index,htm, or index.shtml, then checking this will redirect any of these to your blog's main page and not show a 404 page not found. Useful for websites that previously had an index page.	</td>
+</tr>
+<tr valign="middle">
+	<td>Fix Blogger Labels </td>
+	<td><input name="kpg_pf_labels" type="checkbox" value="Y" <?php if ($kpg_pf_labels=='Y') {?> checked="checked" <?php } ?>/></td>
+	<td>Blogger.com uses the url &quot;/labels/&quot; folder instead of categories. If you have imported your site from Blogger.com, you can check off this option to automatically redirect links from /labels/string to /category/string. </td>
+</tr>
+<tr>
+<td>Track 404 and redirects</td>
+<td><select name="kpg_pf_stats">
+    <option value="0" <?php if ($kpg_pf_stats=='0') {?> selected="selected" <?php } ?>>Disabled</option>
+    <option value="10" <?php if ($kpg_pf_stats=='10') {?> selected="selected" <?php } ?>>Last 10</option>
+    <option value="20" <?php if ($kpg_pf_stats=='20') {?> selected="selected" <?php } ?>>Last 20</option>
+    <option value="30" <?php if ($kpg_pf_stats=='30') {?> selected="selected" <?php } ?>>Last 30</option>
+</select></td>
+	<td>As long as we are looking at 404&apos;s and trying to redirect them we might as well keep track of the last few hits and what happened to them. You can keep up to 30 of the last hits in memory. (If you set this to zero you will lose any statistics that have been recorded.)	</td>
+</tr>
+<?php
+	if (function_exists('switch_to_blog')) {
+		if ($blog_id==1) {
+?>
+<tr>
+  <td>Only show configuration on main blog (MU Only) </td>
+  <td><input name="kpg_pf_mu" type="checkbox" value="Y" <?php if ($kpg_pf_mu=='Y') {?> checked="checked" <?php } ?>/></td>
+  <td>This option lnly applies to MU blogs. If this plugin is installed for all blogs on the network, then only the Blog #1 can see and change these options. Statistics can only be read on the main blog. </td>
+</tr>
+<?php
+	}
+}
+?>
+</table>
+<p class="submit">
+<input type="submit" name="Submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+</p>
+</form>
+<?php 
+	if ($kpg_pf_stats>0) {
+		if (count($f404)>0) {
+?>
+<h3 align="center">Fixed Permalinks</h3>
+<table align="center" cellspacing="2" style="background-color:#CCCCCC;">
+<tr>
+<td style="background-color:#FFFFFF">Date/Time</td>
+<td style="background-color:#FFFFFF">Requested Page</td>
+<td style="background-color:#FFFFFF">Fixed Permalink</td>
+<td style="background-color:#FFFFFF">Referring Page</td>
+<td style="background-color:#FFFFFF">Browser User Agent</td>
+<td style="background-color:#FFFFFF">Remote IP</td>
+
+</tr>
+<?php
+for ($j=0;$j<count($f404)&&$j<$kpg_pf_stats;$j++ ) {
+    $f404[$j][1]=urldecode($f404[$j][1]);
+    $f404[$j][5]=urldecode($f404[$j][5]);
+    $f404[$j][2]=urldecode($f404[$j][2]);
+    $f1=$f404[$j][1];
+    $f5=$f404[$j][5];
+    $f2=$f404[$j][2];
+	if (strlen($f1)>32) $f1=substr($f1,0, 32).'...';
+	if (strlen($f5)>32) $f5=substr($f5,0,32).'...';
+	if (strlen($f2)>32) $f2=substr($f2,0,32).'...';
+?>
+<tr>
+<td style="background-color:#FFFFFF"><?php echo $f404[$j][0]; ?></td>
+<td style="background-color:#FFFFFF"><a href="<?php echo $f404[$j][1]; ?>" title="<?php echo $f404[$j][1]; ?>" target="_blank"><?php echo $f1; ?></a></td>
+<td style="background-color:#FFFFFF"><a href="<?php echo $f404[$j][5]; ?>" title="<?php echo $f404[$j][5]; ?>" target="_blank"><?php echo $f5; ?></a></td>
+<td style="background-color:#FFFFFF"><a href="<?php echo $f404[$j][2]; ?>" title="<?php echo $f404[$j][2]; ?>" target="_blank"><?php echo $f2; ?></a></td>
+<td style="background-color:#FFFFFF"><?php echo $f404[$j][3]; ?></td>
+<td style="background-color:#FFFFFF"><?php echo $f404[$j][4]; ?></td>
+</tr>
+<?php } ?>
+</table>
+<?php } ?>
+<?php 
+	if (count($e404)>0) {
+?>
+<h3 align="center">404 errors</h3>
+<table align="center" cellspacing="2" style="background-color:#CCCCCC;">
+<tr>
+<td style="background-color:#FFFFFF">Date/Time</td>
+<td style="background-color:#FFFFFF">Requested Page</td>
+<td style="background-color:#FFFFFF">Referring Page</td>
+<td style="background-color:#FFFFFF">Browser User Agent</td>
+<td style="background-color:#FFFFFF">Remote IP</td>
+</tr>
+<?php
+for ($j=0;$j<count($e404)&&$j<$kpg_pf_stats;$j++ ) {
+    $e404[$j][1]=urldecode($e404[$j][1]);
+    $e404[$j][2]=urldecode($e404[$j][2]);
+    $f1=$e404[$j][1];
+    $f2=$e404[$j][2];
+	if (strlen($f1)>32) $f1=substr($f1,0, 32).'...';
+	if (strlen($f2)>32) $f2=substr($f2,0,32).'...';
+?>
+<tr>
+<td style="background-color:#FFFFFF"><?php echo $e404[$j][0]; ?></td>
+<td style="background-color:#FFFFFF"><a href="<?php echo $e404[$j][1]; ?>" title="<?php echo $e404[$j][1]; ?>" target="_blank"><?php echo $f1; ?></td>
+<td style="background-color:#FFFFFF"><a href="<?php echo $e404[$j][2]; ?>" title="<?php echo $e404[$j][2]; ?>" target="_blank"><?php echo $f2; ?></a></td>
+<td style="background-color:#FFFFFF"><?php echo $e404[$j][3]; ?></td>
+<td style="background-color:#FFFFFF"><?php echo $e404[$j][4]; ?></td>
+</tr>
+<?php } ?>
+</table>
+
+  <?php
+		}
+	} // end if kpg_pf_stats>0
+?>
+  <br/>
+
+
+<hr/>
+<p>Keith Graham is also the Author a collection of Science Fiction Stories. If you find this plugin useful, you might like to to <strong>Buy the Book</strong>.</p>
+<p><a href="http://www.amazon.com/gp/product/1456336584?ie=UTF8&tag=thenewjt30page&linkCode=as2&camp=1789&creative=390957&creativeASIN=1456336584">Error Message Eyes: A Programmer's Guide to the Digital Soul</a><img src="http://www.assoc-amazon.com/e/ir?t=thenewjt30page&l=as2&o=1&a=1456336584" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
+</p>
+<p>This plugin is free and I expect nothing in return. 
+  A link on your blog to one of my personal sites would be appreciated.</p>
+<p>Keith Graham</p>
+<p>
+	<a href="http://www.blogseye.com" target="_blank">Blog&apos;s Eye</a> (My Wordpress Plugins and other PHP coding projects) <br />
+<a href="http://www.cthreepo.com/blog" target="_blank">Wandering Blog </a>(My personal Blog) <br />
+	<a href="http://www.cthreepo.com" target="_blank">Resources for Science Fiction</a> (Writing Science Fiction) <br />
+	<a href="http://www.jt30.com" target="_blank">The JT30 Page</a> (Amplified Blues Harmonica) <br />
+	<a href="http://www.harpamps.com" target="_blank">Harp Amps</a> (Vacuum Tube Amplifiers for Blues) <br />
+	<a href="http://www.cthreepo.com/bees" target="_blank">Bee Progress Beekeeping Blog</a> (My adventures as a new beekeeper) </p>
+</div>
+
+<p>&nbsp;</p>
+
+</p>
---
+++ permalink-finder.php
@@ -0,0 +1,258 @@
+<?php
+/*
+Plugin Name: Permalink Finder
+Plugin URI: http://www.BlogsEye.com/
+Description: Never get a 404 page not found again. If you have restructured or moved your blog, this plugin will find the right post or page every time.
+Version: 1.50
+Author: Keith P. Graham
+Author URI: http://www.BlogsEye.com/
+
+This software is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+
+  
+/************************************************************
+* 	kpg_permalink_finder()
+*	This is kicked off from the template_redirect event.
+*	If we arrived here through a 404 error, parse the URL	
+*	and try to find a page as much like the bad one as
+*	possible.
+*************************************************************/
+function kpg_permalink_finder() {
+	if( is_404() ) {
+		// get the repository flags and data
+		$kpg_pf_find='2'; 
+		$kpg_pf_index='N';
+		$kpg_pf_stats='0';
+		$kpg_pf_labels='N';
+		$kpg_pf_mu='Y';
+		$e404=array();
+		$f404=array();
+		$options=kpg_pf_get_global_option('kpg_permalinfinder_options');
+		if (!is_array($options)) $options=array();
+		if (array_key_exists('mu',$options) ) $kpg_pf_mu=$options['mu'];
+		if ($kpg_pf_mu=='N') $options=get_option('kpg_permalinfinder_options');
+		if ($options==null) $options=array();
+		if (!is_array($options)) $options=array();
+		if (array_key_exists('find',$options) ) $kpg_pf_find=$options['find'];
+		if (array_key_exists('index',$options) ) $kpg_pf_index=$options['index'];
+		if (array_key_exists('stats',$options) ) $kpg_pf_stats=$options['stats'];
+		if (array_key_exists('labels',$options) ) $kpg_pf_labels=$options['labels'];
+		// check data and set defaults
+		if ($kpg_pf_find!='9999' && $kpg_pf_find!='1' && $kpg_pf_find!='2' && $kpg_pf_find!='3' && $kpg_pf_find!='4') {
+			$kpg_pf_find='2';
+		}
+		if ($kpg_pf_index!='Y' && $kpg_pf_index!='N') $kpg_pf_index='N';
+		if ($kpg_pf_labels!='Y' && $kpg_pf_labels!='N') $kpg_pf_labels='N';
+		if ($kpg_pf_stats!='10' && $kpg_pf_stats!='20' && $kpg_pf_stats!='30') {
+			$kpg_pf_stats='0';
+		}
+		if (array_key_exists('e404',$options) ) $e404=$options['e404']; 
+		if (array_key_exists('f404',$options) ) $f404=$options['f404']; 
+		
+		// record any pertanent data
+		if ($kpg_pf_stats>'0') {
+			$r404=array();
+				$r404[0]=date('m/d/Y H:i:s');
+				$r404[1]=esc_url($_SERVER['REQUEST_URI']);
+				$r404[2]=esc_url_raw(html_entity_decode($_SERVER['HTTP_REFERER']));
+				$r404[3]=htmlentities($_SERVER['HTTP_USER_AGENT']);
+				$r404[4]=$_SERVER['REMOTE_ADDR'];
+		}
+
+		$plink = basename( $_SERVER['REQUEST_URI'] ); // flink has full url that was missed
+		// check if the incoming line needs a blogger fix
+		if ($kpg_pf_labels=='Y') { 
+			$flink = $_SERVER['REQUEST_URI']; // plink has the page that was 404'd	
+			if (strpos($flink,'/labels/')>0) {
+				$flink=urldecode($flink);
+				$flink=remove_accents($flink);
+				$flink=str_replace('/labels/','/category/',$flink);
+				$flink=str_replace('.html','',$flink); // get dir of html and shtml at the end - don't need to search for these
+				$flink=str_replace('.shtml','',$flink); 
+				$flink=str_replace('.htm','',$flink); 
+				$flink=sanitize_url($flink);
+
+				if ($kpg_pf_stats>'0') {
+					$r404[5]=$flink;
+					array_unshift($f404,$r404);
+					for ($j=0;$j<10;$j++) {
+						$n=count($f404);
+						if ($n>$kpg_pf_stats) {
+							array_pop($f404);
+						}
+					}
+					$options['f404']=$f404;
+					if ($kpg_pf_mu=='N') {
+						update_option('kpg_permalinfinder_options', $options);
+					} else {
+						kpg_pf_set_global_option('kpg_permalinfinder_options', $options);
+					}
+					
+				}
+				wp_redirect($flink,"301"); // let wp do it - more compatable.
+				exit();
+			}
+		}
+		
+		// check to see if the user is coming in on a base default
+		if ($kpg_pf_index=='Y') { // quick check to see if we are accessing an index page
+			if ($plink=='index.html'||$plink=='index.htm'||$plink=='index.shtml'||$plink=='default.asp') {
+				if ($kpg_pf_stats>'0') {
+					$r404[5]=get_bloginfo('url');
+					array_unshift($f404,$r404);
+					for ($j=0;$j<10;$j++) {
+						$n=count($f404);
+						if ($n>$kpg_pf_stats) {
+							array_pop($f404);
+						}
+					}
+					$options['f404']=$f404;
+					if ($kpg_pf_mu=='N') {
+						update_option('kpg_permalinfinder_options', $options);
+					} else {
+						kpg_pf_set_global_option('kpg_permalinfinder_options', $options);
+					}
+				}
+				wp_redirect(get_bloginfo('url'),"301"); // let wp do it - more compatable.
+				exit();
+			}
+		}
+
+		// now figure if we need to fix a permalink
+		if ($kpg_pf_find<5) {
+			$ID = kpg_find_permalink_post( $plink,$kpg_pf_find );
+			if( $ID>0 )  { //check for match	
+				if ($kpg_pf_stats>'0') {
+					$r404[5]=get_permalink( $ID );
+					array_unshift($f404,$r404);
+					for ($j=0;$j<10;$j++) {
+						$n=count($f404);
+						if ($n>$kpg_pf_stats) {
+							array_pop($f404);
+						}
+					}
+					$options['f404']=$f404;
+					if ($kpg_pf_mu=='N') {
+						update_option('kpg_permalinfinder_options', $options);
+					} else {
+						kpg_pf_set_global_option('kpg_permalinfinder_options', $options);
+					}
+				} 
+				wp_redirect(get_permalink( $ID ),"301"); // let wp do it - more compatable.
+				exit();
+			}
+		}
+		// still here, it must be a real 404, we should log it
+		if ($kpg_pf_stats>'0') {
+			array_unshift($e404,$r404);
+			for ($j=0;$j<10;$j++) {
+				$n=count($e404);
+				if ($n>$kpg_pf_stats) {
+					unset($e404[$n-1]);
+				}
+			}
+			$options['e404']=$e404;
+				if ($kpg_pf_mu=='N') {
+					update_option('kpg_permalinfinder_options', $options);
+				} else {
+					kpg_pf_set_global_option('kpg_permalinfinder_options', $options);
+				}
+		}
+	}
+}
+
+/************************************************************
+*	kpg_permalink_finder( $plink )
+*	$plink is the permalink value from the url
+*	does a lookup on the posts table in order to find the post
+*	if not found in posts it tries the pages table.
+*************************************************************/
+function kpg_find_permalink_post( $plink,$kpg_pf_find ) {
+	global $wpdb; // useful db functions
+	// fix up the link - NEW - use the wordpress sanitize link 
+	$plink=' '.urldecode($plink); // have no idea why I need this space here.
+	$plink = remove_accents($plink);
+	$plink=strtolower($plink); // make it case insensitive
+	$plink=str_replace('.html','',$plink); // get dir of html and shtml at the end - don't need to search for these
+	$plink=str_replace('.shtml','',$plink); 
+	$plink=str_replace('.htm','',$plink); 
+	$plink=str_replace('.php','',$plink); 
+	
+	$plink=str_replace(' ','-',$plink); 
+	$plink=sanitize_url($plink);
+	// first check to see if it is a good slug already - without the fuzzy search
+	$post = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE post_name = %s AND post_status = 'publish'", $plink ));
+	if ( $post ) return $post;
+
+	$ss=explode("-",$plink); // place into an arrary
+	// look for each word in the array. If found add in 1; if not add in 0. Order by sum and the best bet bubbles to top.
+	$sql="SELECT ID, ";
+	for ($j=0;$j<count($ss);$j++) {
+		$sql=$sql." if(INSTR(LCASE(post_name),'".mysql_real_escape_string($ss[$j])."'),1,0)+" ;
+	}
+	$sql=$sql."0 as CNT FROM ".$wpdb->posts." WHERE post_status = 'publish' ORDER BY CNT DESC, POST_DATE";
+	$row=$wpdb->get_row($sql);
+	if ($row) {	
+	   $ID=$row->ID; 
+	   $CNT=$row->CNT;
+	   if ($CNT>=$kpg_pf_find) return $ID;
+	} 
+	return 0;
+}
+
+// use this to find out global options
+function kpg_pf_get_global_option($option) {
+	// this gets the plugin control - never globalized (I hope)
+	if (!function_exists('switch_to_blog')) return get_option($option);
+	switch_to_blog(1); 
+	$ansa=get_option($option);
+	restore_current_blog();
+	return $ansa;  
+}
+function kpg_pf_set_global_option($option,$value) {
+	// this gets the plugin control - never globalized (I hope)
+	if (!function_exists('switch_to_blog')) return update_option($option,$value);
+	switch_to_blog(1); 
+	$ansa=update_option($option, $value);
+	restore_current_blog();
+	return $ansa;  
+}
+
+function kpg_permalink_finder_uninstall() {
+	if(!current_user_can('manage_options')) {
+		die('Access Denied');
+	}
+	delete_option('kpg_permalinfinder_options'); 
+	return;
+}
+
+
+/************************************************************
+*	kpg_permalink_finder_admin_menu()
+*	Adds the admin menu
+*************************************************************/
+function kpg_permalink_finder_admin_menu() {
+// check to see if we are in MU and the options have been set to the exclusively MU blog 1
+	global $blog_id;
+	$kpg_pf_mu='Y';
+	$options=kpg_pf_get_global_option('kpg_permalinfinder_options');
+	if (!is_array($options)) $options=array();
+	if (array_key_exists('mu',$options)) $kpg_pf_mu=$options['mu'];
+	if ($kpg_pf_mu=='N'||$blog_id==1||(!function_exists('switch_to_blog'))) {
+		add_options_page('Permalink Finder', 'Permalink Finder', 'manage_options', 'permalink-finder/permalink-finder-options.php');
+	} 
+}
+
+add_action( 'template_redirect', 'kpg_permalink_finder' );
+// add the the options to the admin menu
+
+add_action('admin_menu', 'kpg_permalink_finder_admin_menu');
+if ( function_exists('register_uninstall_hook') ) {
+	register_uninstall_hook(__FILE__, 'kpg_permalink_finder_uninstall');
+}
+
+?>
